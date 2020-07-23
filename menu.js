a914f9ef2cf72c5afb5845a9f85be926e12e3b76
souleymane <souleymane.conde@capgemini.com>
2020-07-23 16:47:25 +0200

    Mise à jour du site

---
+++ menu.js
@@ -0,0 +1,12 @@
+
+function addMenu(pageId) {
+    var menuContent = 
+        `<ul>
+            <li><a id= "page_accueil" class="unactive" href="index.html">Accueil</a></li>
+            <li><a id= "page_cv" class="unactive" href="cv.html">CV</a></li>
+            <li><a id= "page_publications" class="unactive" href="publications/index.html">Publications</a></li>
+        </ul>`;
+   
+    document.getElementById('menu').innerHTML = menuContent;
+    document.getElementById(pageId).className = "active";     
+}
