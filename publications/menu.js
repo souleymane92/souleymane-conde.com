417af21a85ba07fd90babaed6c1dbcbe36ec7f3d
souleymane <souleymane.conde@capgemini.com>
2020-07-23 17:10:48 +0200

    Correction des liens du menu

--- publications/menu.js
+++ publications/menu.js
@@ -1,7 +1,7 @@
 function addMenu(pageId) {
     var menuContent = 
         `<ul>
-            <li><a id= "page_accueil" class="unactive" href="../index.html">Accueil</a></li>
+            <li><a id= "page_accueil" class="unactive" href="../accueil.html">Accueil</a></li>
             <li><a id= "page_cv" class="unactive" href="../index.html">CV</a></li>
             <li><a id= "page_publications" class="unactive" href="publication.html">Publications</a></li>
         </ul>`;
