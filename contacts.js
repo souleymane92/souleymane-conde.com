c0fb377e2330c1a69f6654a0ab26d56207285c06
SOULEYMANE CONDE <49903942+souleymane92@users.noreply.github.com>
2024-04-28 18:54:57 +0200

    Update and rename contacts.html to contacts.js

---
+++ contacts.js
@@ -0,0 +1,7 @@
+function addCoordonnees() {
+    var coordonnees = 
+        <br><p>Adresse email: condesouleymane98@gmail.com</p><br>
+	<p>Adresse: Cergy, 95000 - France</p>`;
+   
+    document.getElementById('coordonnees').innerHTML = coordonnees;
+}
