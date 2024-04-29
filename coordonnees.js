4c9eb464d958496e5410ab41642e38fa64534d55
SOULEYMANE CONDE <49903942+souleymane92@users.noreply.github.com>
2024-04-29 16:27:52 +0200

    Delete coordonnees.js

--- coordonnees.js
+++
@@ -1,7 +0,0 @@
-function addCoordonnees() {
-    var coordonnees = 
-        `<br><p>Adresse email: condesouleymane98@gmail.com</p><br>
-	<p>Adresse: Cergy, 95000 - France</p>`;
-   
-    document.getElementById('coordonnees').innerHTML = coordonnees;
-}
